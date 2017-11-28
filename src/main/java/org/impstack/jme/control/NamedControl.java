a7789a05a5368a5c198de463158d8fd72530dbb7
Remy Van Doosselaer <remy@impstack.org>
2017-11-28 11:01:02 +0100

    add a NamedControl iface

---
+++ src/main/java/org/impstack/jme/control/NamedControl.java
@@ -0,0 +1,13 @@
+package org.impstack.jme.control;
+
+import com.jme3.scene.control.Control;
+
+/**
+ * @author remy
+ * @since 28/11/17.
+ */
+public interface NamedControl extends Control {
+
+    String getName();
+
+}
